e9a4fda006f746d17b61b92712c7d521f64a4b6d
Sarah Peng <Sarah@Sarah-Pengs-MacBook.local>
2019-11-18 18:47:59 -0800

    plot3

---
+++ plot3.R
@@ -0,0 +1,22 @@
+library(ggplot2)
+library(tidyverse)
+library(dplyr)
+library(leaflet)
+library(knitr)
+library(plotly)
+
+df <- read.csv("data/children_fatalities.csv", stringsAsFactors = FALSE)
+
+histogram_plot <- plot_ly(df,
+                          x = ~State, y = ~X2016.Total.Child.Fatalities, type = "bar",
+                          name = "2016 Child Fatalities"
+) %>%
+  add_trace(y = ~X2017.Total.Child.Fatalities, name = "2017 Child Fatalities") %>%
+  layout(
+    title = "Children Fatalities per State in 2017",
+    xaxis = list(title = "States", tickangle = -45),
+    yaxis = list(title = "# of Child Fatalities"),
+    margin = list(b = 100),
+    barmode = "stack"
+  )
+histogram_plot
