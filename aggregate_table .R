d685d968c66f9635bb4db911b0b67607dccf3079
cassiemao <56013849+cassiemao@users.noreply.github.com>
2019-11-18 23:16:18 -0800

    Add files via upload

--- aggregate_table .R
+++ aggregate_table .R
@@ -1,17 +1,20 @@
 library(dplyr)
-dataset <- read.csv(file = "./data/children fatalities.csv",
+children_fatalities <- read.csv(file = "./data/children_fatalities.csv",
                     stringsAsFactors = FALSE)
 
 summary_table <- function(dataset) {
-                table <- dataset %>% 
-                         group_by(State) %>%
-                         select(State, X2013.Total.Child.Fatalities, X2014.Total.Child.Fatalities,
-                                X2015.Total.Child.Fatalities) %>%
-                         summarise(year_2013_to_2015 = sum(X2013.Total.Child.Fatalities) 
-                                                       + sum(X2014.Total.Child.Fatalities) 
-                                                       + sum(X2015.Total.Child.Fatalities)) %>% 
-                         arrange(-year_2013_to_2015) %>% 
-                         head(7)
+                    table <- dataset %>% 
+                    group_by(State) %>%
+                    select(State, X2013.Total.Child.Fatalities, X2014.Total.Child.Fatalities,
+                           X2015.Total.Child.Fatalities) %>%
+                    filter(State != "National") %>% 
+                    summarise(year_2013_to_2015 = (sum(as.numeric(X2013.Total.Child.Fatalities))
+                                                   + sum(as.numeric(X2014.Total.Child.Fatalities))
+                                                   + sum(as.numeric(X2015.Total.Child.Fatalities))))%>% 
+                    arrange(-year_2013_to_2015) %>% 
+                    head(7)
                     
                 return(table)            
 } 
+
+aggregate_table <- summary_table(children_fatalities)
