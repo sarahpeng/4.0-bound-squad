e5cdd1ed62f7dd2897b870398b4c24af35ac1d8d
cassiemao <56013849+cassiemao@users.noreply.github.com>
2019-11-19 00:58:20 -0800

    Summary Table

--- aggregate_table .R
+++ aggregate_table .R
@@ -2,19 +2,20 @@ library(dplyr)
 children_fatalities <- read.csv(file = "./data/children_fatalities.csv",
                     stringsAsFactors = FALSE)
 
-summary_table <- function(dataset) {
-                    table <- dataset %>% 
-                    group_by(State) %>%
-                    select(State, X2013.Total.Child.Fatalities, X2014.Total.Child.Fatalities,
-                           X2015.Total.Child.Fatalities) %>%
-                    filter(State != "National") %>% 
-                    summarise(year_2013_to_2015 = (sum(as.numeric(X2013.Total.Child.Fatalities))
-                                                   + sum(as.numeric(X2014.Total.Child.Fatalities))
-                                                   + sum(as.numeric(X2015.Total.Child.Fatalities))))%>% 
-                    arrange(-year_2013_to_2015) %>% 
-                    head(7)
+summary_table <-  function(dataset) {
                     
-                return(table)            
+                  table <- dataset %>% 
+                  group_by(State) %>%
+                  select(State, X2013.Total.Child.Fatalities, X2014.Total.Child.Fatalities,
+                         X2015.Total.Child.Fatalities) %>%
+                  filter(State != "National") %>% 
+                  summarise(year_2013_to_2015 = (sum(as.numeric(X2013.Total.Child.Fatalities))
+                                                 + sum(as.numeric(X2014.Total.Child.Fatalities))
+                                                 + sum(as.numeric(X2015.Total.Child.Fatalities)))) %>% 
+                  arrange(-year_2013_to_2015) %>% 
+                  head(7)
+                    
+                  return(table)            
 } 
 
 aggregate_table <- summary_table(children_fatalities)
